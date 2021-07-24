434b3cef51c98a31798d6101a52ad7ba9360a374
bangfu-tao <bangfu.tao@samsung.com>
2021-07-24 20:43:07 +0100

    plots TA: macd, sma

--- python/fx/backtest.py
+++ python/fx/backtest.py
@@ -1,45 +1,100 @@
 from datetime import datetime, timedelta
 from zipfile import ZipFile
 import pandas as pd
+import pandas_ta as ta
 import numpy as np
 from matplotlib.widgets import Cursor
 import matplotlib.pyplot as plt
 import mplcursors
+from dateutil import parser
+import enum
+
 
 import time, requests
 import os, sys, io
 import glob
 
 
-
-def main2():
-    fig = plt.figure()
-    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-
-    num = 100
-    x = np.random.rand(num)
-    y = np.random.rand(num)
-
-    ax.scatter(x, y, c='blue')
-    ax.set_xlabel('X-axis')
-    ax.set_ylabel('Y-axis')
-
-    cursor = Cursor(ax, color='green', linewidth=2)
-    plt.show()
-
-
 def main():
-    dataStatsManager = DataStatsManager()
-    dataStatsManager.ticker_stats_calc('EURUSD')
+   dataStatsManager = DataStatsManager()    
+   dataStatsManager.ticker_stats_calc('EURUSD')
 
 
 ###############################################################################
+class OrderType(enum.Enum):
+    NONE = 0
+    LONG = 1
+    SHORT = 2
+    EXIT = 3
+    CANCEL = 4
+        
+class PositionType(enum.Enum):
+    NONE = 0
+    LONG = 1
+    SHORT = 2
+
+class ForexSize(enum.Enum):
+    STANDARD_LOT = 100000
+    MINI_LOT = 10000
+    MICRO_LOT = 1000
+
+class Broker():
+    def __init__(self, df, size = ForexSize.STANDARD_LOT):
+        self.df = df
+        self.size = size
+        self.order = OrderType.NONE         # requesting only
+        self.postion = PositionType.NONE    # filled / positioned
+        self.orders = []
+        self.orders_df = pd.DataFrame()
+
+    def excute_order(self, price, df_index):
+        if self.order == OrderType.EXIT:
+            if len(self.orders) > 0:
+                order = self.orders[-1]
+                order['exit_price'] = price
+                delt = (order['exit_price'] - order['entry_price'])
+                if (order['order_type'] == OrderType.SHORT):
+                    delt = - delt
+                order['profit_loss'] = delt * self.size  # convert pips to money
+                order['exit_index'] = df_index
+                self.orders[-1] = order
+            self.order = OrderType.NONE
+            return
+
+        if self.order == OrderType.LONG:
+            dict = {'order_type': OrderType.LONG, 'entry_price': price, 'exit_price': 0, 'profit_losss': 0, 'entry_index': df_index, 'exit_index': 0}
+            self.orders.append(dict)
+            self.order = OrderType.NONE
+            self.postion = PositionType.LONG
+            return
+
+        if self.order == OrderType.SHORT:
+            dict = {'order_type': OrderType.SHORT, 'entry_price': price, 'exit_price': 0, 'profit_losss': 0, 'entry_index': df_index, 'exit_index': 0}
+            self.orders.append(dict)
+            self.order = OrderType.NONE
+            self.postion = PositionType.SHORT
+            return
+        
+
+    def send_order(self, type):
+        if (self.order != OrderType.NONE):
+            return False
+        if (self.postion != PositionType.NONE):
+            return False
+        self.order = type
+        return True
+
+    def get_orders_df(self):
+        self.orders_df = pd.DataFrame(self.orders)
+        return self.orders_df
+
+
 class SecurityInfo():
     def __init__(self, ticker_name = ""):
         self.ticker_name = ticker_name
         self.csv_file_name = "data/datasets/"+ticker_name+'.csv'
-        self.stats = {}
         self.df = {}
+        self.broker = Broker(self.df)
 
     def load_dataframe(self, df_header):
         self.df = pd.read_csv(self.csv_file_name)
@@ -129,19 +184,50 @@ class DataStatsManager():
         #print(security.stats)
         #security.stats = "TO DO"
         #print(security.stats)
-        print(security.df.head(10))
-        print(security.df.loc[3, ['Date', 'Time']])
-        row = security.df.loc[3, ['Date', 'Time']]
+        #security.df['ema200'] = ta.ema(security.df['Close'], length=200)
+        #security.df['ema400'] = ta.ema(security.df['Close'], length=200*2)
+        #security.df['ema600'] = ta.ema(security.df['Close'], length=200*3)
+        #security.df['ema800'] = ta.ema(security.df['Close'], length=200*4)
+        #security.df['ema1000'] = ta.ema(security.df['Close'], length=200*5)
+        #security.df['ema1200'] = ta.ema(security.df['Close'], length=200*6)
+        #security.df['ema3000'] = ta.ema(security.df['Close'], length=50*60)
+        security.df['sma_long'] = ta.sma(security.df['Close'], length=50*60)
+        security.df['sma_mid'] = ta.sma(security.df['Close'], length=50*15)
+        macd_df = ta.macd(security.df['Close'], 12*5, 26*5)
+        #print(macd_df)
+        """
+                MACD_12_26_9  MACDh_12_26_9  MACDs_12_26_9
+        0                NaN            NaN            NaN
+        1                NaN            NaN            NaN
+        2                NaN            NaN            NaN
+        3                NaN            NaN            NaN
+        4                NaN            NaN            NaN
+        ...              ...            ...            ...
+        932300     -0.000013      -0.000007      -0.000006
+        932301     -0.000014      -0.000006      -0.000008
+        932302     -0.000014      -0.000005      -0.000009        
 
-        print("row:")
-        print(row)
+        """
+        security.df['macd_line'] = macd_df.iloc[:, 2]  # diff
+        security.df['macd_signal'] = ta.ema(security.df['macd_line'], length=9*5) # extra filter
+        security.df['macd_histogram'] = security.df['macd_line'] - security.df['macd_signal']
 
-        d = security.df.at[3, 'Date']
-        t = security.df.at[3, 'Time']
-        print(f"{d}-{t}")
+        #print(security.df.head(10))
+        print(security.df)
 
-        print(security.df.loc[3, ['Date']] )
-        print(security.df.loc[3, ['Time']] )
+
+        #print(security.df.loc[3, ['Date', 'Time']])
+        #row = security.df.loc[3, ['Date', 'Time']]
+
+        #print("row:")
+        #print(row)
+
+        #d = security.df.at[3, 'Date']
+        #t = security.df.at[3, 'Time']
+        #print(f"{d}-{t}")
+
+        #print(security.df.loc[3, ['Date']] )
+        #print(security.df.loc[3, ['Time']] )
         #security.df['Close'].plot(style='.-', title=f"{ticker_symbol}")
         #plt.grid()
         #plt.show()
@@ -154,24 +240,49 @@ class DataStatsManager():
         #self.df_15m = self.resample('15m')
         #self.df_5m = self.resample('5m')
 
-        fig, ax = plt.subplots()
+        fig, (ax1, ax2) = plt.subplots(2, 1)
         #ax = fig.add_axes(security.df['Close'])
         #ax.plot(list(security.df.index.values), security.df['Close'], linestyle='-.')
         #cursor = Cursor(ax, color='pink', linewidth=0.5)
-        #ax.plot(security.df.index[0:24*60*100], security.df['Close'][0:24*60*100], linestyle='-', marker='.')
-        ax.plot(security.df.index[0:], security.df['Close'][0:], linestyle='-', marker='.')
+        df_size = security.df.shape[0]
+        data_start = df_size - 100*24*60;
+        data_end = df_size
+        print(f"df_size={df_size}")
+        ax1.plot(security.df.index[data_start:data_end], security.df['Close'][data_start:data_end], linestyle='-', marker='.', label='price')
+        #ax.plot(security.df.index[0:24*60*num_days+1], security.df['ema200'][0:24*60*num_days+1], linestyle='-', marker='.', label='ema200')
+        #ax.plot(security.df.index[0:24*60*num_days+1], security.df['ema400'][0:24*60*num_days+1], linestyle='-', marker='.', label='ema400')
+        #ax.plot(security.df.index[0:24*60*num_days+1], security.df['ema600'][0:24*60*num_days+1], linestyle='-', marker='.', label='ema600')
+        #ax.plot(security.df.index[0:24*60*num_days+1], security.df['ema800'][0:24*60*num_days+1], linestyle='-', marker='.', label='ema800')
+        #ax.plot(security.df.index[0:24*60*num_days+1], security.df['ema1000'][0:24*60*num_days+1], linestyle='-', marker='.', label='ema1000')
+        #ax.plot(security.df.index[0:24*60*num_days+1], security.df['ema1200'][0:24*60*num_days+1], linestyle='-', marker='.', label='ema1200')
+        #ax.plot(security.df.index[0:24*60*num_days+1], security.df['ema3000'][0:24*60*num_days+1], linestyle='-', marker='.', label='ema3000')
+        ax1.plot(security.df.index[data_start:data_end], security.df['sma_long'][data_start:data_end], linestyle='-', marker='.',  linewidth=0.5, label='sma_long')
+        ax1.plot(security.df.index[data_start:data_end], security.df['sma_mid'][data_start:data_end], linestyle='-', marker='.',  linewidth=0.5, label='sma_mid')
+
+        ax2.plot(security.df.index[data_start:data_end], security.df['macd_line'][data_start:data_end], linestyle='-', marker='.',  linewidth=0.5, label='macd_line')
+        ax2.plot(security.df.index[data_start:data_end], security.df['macd_signal'][data_start:data_end], linestyle='-', marker='.',  linewidth=0.5, label='macd_signal')
+        #ax2.bar(security.df.index[data_start:data_end], security.df['macd_histogram'][data_start:data_end], label='macd_histogram')
+        #ax.plot(security.df.index[0:], security.df['Close'][0:], linestyle='-', marker='.')
         #cursor = Cursor(ax, color='m', linewidth=0.25)
 
-        crs = mplcursors.cursor(ax,hover=True)
-        #crs.connect("add", lambda sel: sel.annotation.set_text('Point {},{}'.format(sel.target[0], sel.target[1])))
+        crs1 = mplcursors.cursor(ax1, hover=True)
+        crs1.connect("add", lambda sel: sel.annotation.set_text(f"{parser.parse(str(security.df.at[int(sel.target[0]), 'Date'])).strftime('%a')}, {str(security.df.at[int(sel.target[0]), 'Date'])[2:]}-{str(security.df.at[int(sel.target[0]), 'Time']).zfill(6)} {str(int(sel.target[1]*10000)/10000)}"))
+        ax1.set_title(f"{ticker_symbol}")
+        ax1.grid(True)
+        ax1.legend()
+
+        crs2 = mplcursors.cursor(ax2, hover=True)
+        crs2.connect("add", lambda sel: sel.annotation.set_text(f"{parser.parse(str(security.df.at[int(sel.target[0]), 'Date'])).strftime('%a')}, {str(security.df.at[int(sel.target[0]), 'Date'])[2:]}-{str(security.df.at[int(sel.target[0]), 'Time']).zfill(6)} {str(int(sel.target[1]*10000)/10000)}"))
+        #print(f"{parser.parse(str(security.df.at[1, 'Date'])).strftime('%a')}");
         
-        crs.connect("add", lambda sel: sel.annotation.set_text(f"{str(security.df.at[int(sel.target[0]), 'Date'])}-{str(security.df.at[int(sel.target[0]), 'Time']).zfill(6)} {str(int(sel.target[1]*10000)/10000)}"))
-        #crs.connect("add", lambda sel: sel.annotation.set_text(f"{sel.target[1]}"))
-        #crs.connect("add", lambda sel: sel.annotation.set_text(f"{sel.target[0]}"))
+        #parser.parse(str(security.df.at[int(sel.target[0]), 'Date'])).strftime("%a")
+
+        
 
 
-        plt.title(f"{ticker_symbol}")
-        plt.grid()
+       
+        ax2.grid(True)
+        ax2.legend()
         plt.show()
 
 
